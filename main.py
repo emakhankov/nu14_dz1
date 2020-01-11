f1b9f71f1eac5f6edda38b1a62ab6555661a04c6
Jenya <emakhankov@gmail.com>
2020-01-11 17:56:25 +0300

    добавлен telegram

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
+import purchasesVGTRK
+import telebot
+from telebot import apihelper
 import json
+import time
+import os
 import pandas as pd
+from collections import defaultdict
 
+TEXT_WAIT_FOR = 'Подождите до формирования сведений'
+
+STAGE_0, STAGE_SEND_BY_NOMER = range(0, 2)
+USER_STATE = defaultdict(lambda: STAGE_0)
+USER_DATA = defaultdict(lambda: {})
 
 
 def get_settings():
@@ -12,87 +21,153 @@ def get_settings():
         return data
 
 
-def create_dataframe(data, url):
+SETTINGS = get_settings()
+TOKEN = SETTINGS['TOKEN']
+OUTPUT_EXCEL = SETTINGS['OUTPUT_EXCEL']
 
-    df = pd.DataFrame(data, columns=['num', 'href', 'customer', 'description', 'price', 'start', 'finish', 'state'])
-    df[['start', 'finish']] = df[['start', 'finish']].apply(pd.to_datetime)
-    df[['price']] = df[['price']].apply(pd.to_numeric)
-    df['href'] = df['href'].apply(lambda x: f'{url}{x}')
+if 'PROXIES' in SETTINGS:
+    PROXIES = SETTINGS['PROXIES']
+    apihelper.proxy = PROXIES
+bot = telebot.TeleBot(TOKEN)
+
+
+def get_state(message):
+    return USER_STATE[message.chat.id]
+
+
+def update_state(message, newstate):
+    USER_STATE[message.chat.id] = newstate
+
+
+def read_from_excel():
+
+    df = pd.read_excel(OUTPUT_EXCEL, sheet_name='Sheet1', index_col=0)
     return df
 
-def get_data(url, rows):
-    print('gathering from:', url)
+def verify_up_to_date(message):
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    div_id_content = soup.find('div', id='content')
-    if div_id_content is None:
-        print('The structure of html is broken. Missing tag <div>, id="content"')
-        raise SystemExit
-
-    div_pager = div_id_content.find('div', class_='pager')
-    next_exists = False
-    for a_href in div_pager.find_all('a'):
-        if 'след' in a_href.text.lower():
-            next_exists = True
-
-    if next_exists:
-
-        table_zebra = div_id_content.find('table', class_='table zebra')
-        if table_zebra is None:
-            print('The structure of html is broken. Missing tag <table class="table zebra">')
-            raise SystemExit
-
-        table_body = table_zebra.find('tbody')
-        if table_body is None:
-            print('The structure of html is broken. Missing tag <body>')
-            raise SystemExit
-
-        for tr_table_body in table_body.find_all('tr'):
-            row_dict = {}
-            td_table_body = tr_table_body.find_all('td')
-            row_dict['num'] = td_table_body[0].a.text
-            row_dict['href'] = td_table_body[0].a.get('href')
-
-            row_dict['description'] = td_table_body[1].a.text
-
-            row_dict['customer'] = td_table_body[2].text
-
-            row_dict['price'] = td_table_body[3].text.replace(',', '.').replace(' ', '')
-
-            row_dict['start'] = td_table_body[4].text
-
-            row_dict['finish'] = td_table_body[5].text
-
-            row_dict['state'] = td_table_body[6].text
-
-            rows.append(row_dict)
-
-        return False
-
+    if not os.path.isfile(OUTPUT_EXCEL):
+        bot.send_message(message.chat.id, TEXT_WAIT_FOR)
+        purchasesVGTRK.get_excel()
     else:
+        time_creation = os.path.getctime(OUTPUT_EXCEL)
+        time_now = time.time()
+        diff_sec = time_now - time_creation
+        if diff_sec > 3600.0:
+            bot.send_message(message.chat.id, TEXT_WAIT_FOR)
+            purchasesVGTRK.get_excel()
 
-        return True
+# Обработчик 1
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+
+    text = '''Добрый день! Я телебот созданный как домашнее задание. Я умею искать закупки ВГТРК на сайте закупок.
+    Для получения списка команд введите команда /help'''
+
+    bot.send_message(message.chat.id, text)
+    update_state(message, STAGE_0)
+
+# Обработчик 2
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+
+    text = '''
+    Доступные команды:
+    /getexcel - Получить последную полную выгрузку в формате EXCEL
+    /getlast n - Получить информацию о последних n закупках (если n не задано 10)
+    /getbynomer nomer - Получить информацию закупке по номеру'''
+
+    bot.send_message(message.chat.id, text)
+    update_state(message, STAGE_0)
+
+# Обработчик 3
+@bot.message_handler(commands=['getexcel'])
+def send_excel(message):
+
+    verify_up_to_date(message)
+    doc = open(OUTPUT_EXCEL, 'rb')
+    bot.send_document(message.chat.id, doc, caption='Данные в формате EXCEL')
+    update_state(message, STAGE_0)
 
 
-if __name__ == '__main__':
+# Обработчик 4
+@bot.message_handler(commands=['getlast'])
+def send_last(message):
 
-    settings = get_settings()
-    domain_url = settings['DOMAIN_URL']
-    parse_url = settings['PARSE_URL']
-    output_excel = settings['OUTPUT_EXCEL']
+    param = message.text.split()
+    if len(param) == 1:
+        count = 10
+    else:
+        count_t = param[1]
+        if count_t.isdigit():
+            count = int(count_t)
+        else:
+            bot.reply_to(message, 'Ошибка параметра')
+            return
+
+    verify_up_to_date(message)
+    df = read_from_excel()
+    count = min(len(df), count)
+    text = []
+    for index, row in df.head(count).iterrows():
+        if index != 0:
+            text.append('')
+            text.append('')
+        text.append(f'Закупка: {index+1}')
+        text.append(f'Номер: {row["num"]}')
+        text.append(f'ссылка: {row["href"]}')
+        text.append(f'Описание: {row["description"]}')
+        print('\n'.join(text))
+    bot.send_message(message.chat.id, '\n'.join(text))
+    update_state(message, STAGE_0)
 
 
-    print('start gathering info from:', f'{domain_url}{parse_url}')
 
-    i = 0
-    rows = []
-    while True:
-        i += 1
-        return_value = get_data(f'{domain_url}{parse_url}/page/{i}', rows)
-        if return_value:
-            break
+def send_byNomer_finish(message, num):
 
-    df = create_dataframe(rows, domain_url)
-    df.to_excel(output_excel, engine='xlsxwriter')
+    verify_up_to_date(message)
+    df = read_from_excel()
+
+    row = df.loc[df['num'] == num]
+    if len(row) != 0:
+        row = row.iloc[0]
+    else:
+        bot.reply_to(message, 'нет такой закупки')
+    text = []
+    text.append(f'Номер: {row["num"]}')
+    text.append(f'Ссылка: {row["href"]}')
+    text.append(f'Описание: {row["description"]}')
+    text.append(f'Стоимость: {row["price"]}')
+    text.append(f'Состояние: {row["state"]}')
+    bot.send_message(message.chat.id, '\n'.join(text))
+
+# Обработчик 5
+@bot.message_handler(commands=['getbynomer'])
+def send_byNomer(message):
+
+    param = message.text.split()
+    if len(param) < 2:
+        bot.reply_to(message, 'Введите номер закупки')
+        update_state(message, STAGE_SEND_BY_NOMER)
+        return
+
+    num = param[1]
+    send_byNomer_finish(message, num)
+
+
+# Обработчик 6
+@bot.message_handler(func=lambda message: get_state(message) == STAGE_SEND_BY_NOMER)
+def send_byNomer_num(message):
+    num = message.text
+    send_byNomer_finish(message, num)
+    update_state(message, STAGE_0)
+
+# Обработчик 7
+@bot.message_handler(commands=['about'])
+def test_send_message_with_markdown(message):
+    bot.send_message(message.chat.id, "Университет искуственного интеллекта \n Evgeny Makhankov (C)")
+    update_state(message, STAGE_0)
+
+bot.polling()
+
+
--- readme.txt
+++ readme.txt
@@ -3,3 +3,8 @@ http://tendering.vgtrk.com/orders/working
 
 Результат работы сохраняется в файл output.xlsx
 Настройки в файле settings.json
+
+Также работает telegram бот @nu15_VGTRKPurchases_bot
+
+кеширует в течении 1 часа данные запроса с сайту VGTRK в EXCEL файл
+
